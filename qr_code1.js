24f750ee12b084cca424b62430ba352d468ae867
Doston21-wq <rajabovdoston500@gmail.com>
2025-08-10 17:02:42 +0500

    added

--- qr_code1.js
+++ qr_code1.js
@@ -17,41 +17,6 @@ btn.addEventListener("click", () => {
   } else {
     console.log("Input tidak valid");
     qr_code_element.style.display = "none";
-    downl 
-});
-
-function generate(user_input) {
-  qr_code_element.style.display = "";
-  qr_code_element.style.marginTop = "20px";
-  qr_code_element.style.marginBottom = "20px";
-
-  var qrcode = new QRCode(qr_code_element, {
-    text: `${user_input.value}`, 
-    width: 180,  
-    height: 180, 
-    colorDark: "#000000",  
-    colorLight: "#FFFFFF",  
-    correctLevel: QRCode.CorrectLevel.H,
-  });
-  let btn = document.querySelector(".qr-button");
-
-let qr_code_element = document.querySelector(".qr-code");
-
-let downloadLink = document.getElementById("downloadLink");
-
-btn.addEventListener("click", () => {
-  let user_input = document.querySelector("#input_text");
-
-  if (user_input.value !== "") {
-    if (qr_code_element.childElementCount === 0) {
-      generate(user_input);
-    } else {
-      qr_code_element.innerHTML = "";
-      generate(user_input);
-    }
-  } else {
-    console.log("Input tidak valid");
-    qr_code_element.style.display = "none";
     downloadLink.style.display = "none"; 
   }
 });
@@ -69,3 +34,34 @@ function generate(user_input) {
     colorLight: "#FFFFFF",  
     correctLevel: QRCode.CorrectLevel.H,
   });
+
+  setTimeout(() => {
+    let qr_code_canvas = document.querySelector("canvas"); 
+    let paddedCanvas = addPaddingToQRCode(qr_code_canvas, 20); 
+
+    downloadLink.style.display = "block";
+    downloadLink.href = paddedCanvas.toDataURL("image/png");
+    downloadLink.download = "qrcode.png"; 
+  }, 300);
+}
+
+function addPaddingToQRCode(originalCanvas, padding) {
+  let canvas = document.createElement("canvas");
+  canvas.width = originalCanvas.width + padding * 2;
+  canvas.height = originalCanvas.height + padding * 2;
+
+  let context = canvas.getContext("2d");
+  context.fillStyle = "#FFFFFF"; 
+  context.fillRect(0, 0, canvas.width, canvas.height); 
+
+  context.drawImage(originalCanvas, padding, padding);
+
+  return canvas;
+}
+
+
+tippy('#About', {
+  arrow: true, 
+  content: 'Created By ekizr', 
+  animation: 'fade', 
+});
